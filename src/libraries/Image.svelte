008ec35ab7c94823730a2c759e51d2ce469481d4
yongyuth.chuankhuntod <77398391+yongyuth-chuankhuntod@users.noreply.github.com>
2022-07-17 01:50:36 +0700

    :sparkles: feature: added a lazy load library.

---
+++ src/libraries/Image.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import type { ImageType } from "$types/image.type";
+
+    import { onMount } from "svelte";
+
+    import Heart from "$assets/heart_loadding.gif";
+
+    export let actions: ImageType;
+    let image_loaded: boolean = false;
+    let image_failed: boolean = false;
+    let image_loading: boolean = false;
+
+    onMount(() => {
+        const imageObject: HTMLImageElement = new Image;
+        imageObject.src = actions.src;
+        image_loading = true;
+
+        imageObject.onload = () => {
+            image_loading = false;
+            image_loaded = true;
+        }
+        
+        imageObject.onerror = () => {
+            image_loading = false;
+            image_failed = true;
+        }
+    });
+</script>
+
+{#if image_loaded}
+    <img src={actions.src} alt={actions.alt} class={actions.class} />
+{:else if image_failed}
+    <img src={Heart} alt="" class="h-24 w-24 280px:h-20 280px:w-20" />
+{:else if image_loading}
+    <img src={Heart} alt="" class="h-24 w-24 280px:h-20 280px:w-20" />
+{/if}
