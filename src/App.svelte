661b92b7c1aea17f4c16b8e6639b5f05b82f133a
yongyuth.chuankhuntod <77398391+yongyuth-chuankhuntod@users.noreply.github.com>
2022-07-18 10:57:23 +0700

    :sparkles: feature: add route for root webpage and prepare for reached page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
+    import { Router, Route, navigate } from "svelte-navigator";
+    
     import Layout from "$components/Layout.svelte";
-    import Anniversary from "$components/Anniversary.svelte";
+    import Anniversary from "$pages/Anniversary.svelte";
 
     let anniversary_date: number = new Date(2022, 11, 8).getTime();
 </script>
 
-<Layout>
-    <Anniversary actions={
-            {
-                date: anniversary_date,
-                male: "Yongyuth Chuankhuntod",
-                female: "Thanatcha Wongsomsri",
-                repository: "https://github.com/Yothgewalt/our-anniversary"
-            }
-        } 
-    />
-</Layout>
+<Router primary={false}>
+    <Layout>
+        <Route path="/">
+            <Anniversary actions={
+                    {
+                        date: anniversary_date,
+                        male: "Yongyuth Chuankhuntod",
+                        female: "Thanatcha Wongsomsri",
+                        repository: "https://github.com/Yothgewalt/our-anniversary"
+                    }
+                }
+                on:reach="{() => console.log("NEXT_MERGE")}"
+            />
+        </Route>
+    </Layout>
+</Router>
