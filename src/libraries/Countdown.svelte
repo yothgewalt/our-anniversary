32ca93a7f26aad86b9423d04858be3917aef272b
yongyuth.chuankhuntod <77398391+yongyuth-chuankhuntod@users.noreply.github.com>
2022-07-19 16:55:02 +0700

    :sparkles: feature: added a statement when reached anniversary day

--- src/libraries/Countdown.svelte
+++ src/libraries/Countdown.svelte
@@ -2,25 +2,35 @@
     import type { CountdownType } from "$types/countdown.type";
     export let actions: CountdownType;
 
-    import { onMount } from "svelte";
-    import { tweened, type Tweened } from "svelte/motion";
+    import { onMount, createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    import { writable, type Writable } from "svelte/store";
 
     let time_at_now: number = new Date().getTime();
-
-    let difference: Tweened<number> = tweened(actions.date - time_at_now);
+    let time_difference: Writable<number> = writable(actions.date - time_at_now);
     
     onMount(() => {
         setInterval(() => {
-            if ($difference > 0) {
-                $difference -= 1000;
+            if ($time_difference > 0) {
+                $time_difference -= 1000;
             }
         }, 1000);
     });
 
-    $: days = Math.floor($difference / (1000 * 60 * 60 * 24));
-    $: hours = Math.floor(($difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    $: minutes = Math.floor(($difference % (1000 * 60 * 60)) / (1000 * 60));
-    $: seconds = Math.floor(($difference % (1000 * 60)) / 1000);
+    $: days = Math.floor($time_difference / (1000 * 60 * 60 * 24));
+    $: hours = Math.floor(($time_difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    $: minutes = Math.floor(($time_difference % (1000 * 60 * 60)) / (1000 * 60));
+    $: seconds = Math.floor(($time_difference % (1000 * 60)) / 1000);
+    $: {
+        if ($time_difference <= 0) {
+            dispatch("completed");
+        }
+    }
 </script>
 
-{days} Day {hours} Hour {minutes} Minute {seconds} Second
+{#if $time_difference > 0}
+    {days} Day {hours} Hour {minutes} Minute {seconds} Second
+{:else}
+    Finally it's our anniversary.
+{/if}
